6d956dc6b1afa2b2b30e9c7fb1e703d18722bff0
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-02-12 20:55:25 +0900

    page.tsx を更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,20 @@
 //import logo from './logo.svg';
 'use client'
-//import { GamePlayer } from 'koin.js';
-//import 'koin.js/styles.css'; // Included styles
+import { GamePlayer } from 'koin.js';
+import 'koin.js/styles.css'; // Included styles
 
-//export default function App() {
-//  return (
-//    <div style={{ width: '100vw', height: '100vh' }}>
-//      <GamePlayer
-//        romId="game2"
-//        romUrl="/smt2.sfc"
-//        system="SNES"
-//        title="game."
-//      />
-//    </div>
-//  );
-//}
-import React from "react";
-import { Nostalgist } from 'nostalgist'
-
-export default function Page() {
+export default function App() {
   return (
-<main>{await Nostalgist.launch({ core: 'snes', rom: 'smt2.sfc' })}</main>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <GamePlayer
+        romId="game2"
+        romUrl="/smt2.sfc"
+        system="SNES"
+        title="game."
+      />
+    </div>
   );
 }
+
 
 
